8c4d997111f9bf1ae3811414770eb7aa620c56c0
Richard kayan de souza <88511669+Kayansouza@users.noreply.github.com>
2023-03-04 21:52:51 -0300

    first commit

---
+++ JS/login.js
@@ -0,0 +1,27 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form')
+
+
+const validateinput = ({target}) => {
+  if (target.value.length > 2) {
+    button.removeAttribute('disabled');
+    return;
+  }
+
+  button.setAttribute('disabled', '');
+}
+
+const handleSubmit = (event)=>{
+  event.preventDefault();
+
+  localStorage.setItem('player', input.value);
+  window.location = 'Pages/game.html';
+
+}
+
+
+
+
+input.addEventListener('input', validateinput);
+form.addEventListener('submit',handleSubmit);
